e6d3899c1a5e8cc64907317e1293ba65ace8927f
Justin Baerclay <justincbarclay@gmail.com>
2019-10-08 00:52:19 -0600

    Try to modularize fish config, will have to test on new OSs

--- .config/fish/os/linux.fish
+++ .config/fish/os/linux.fish
@@ -6,17 +6,15 @@ set -q XDG_DATA_HOME
 # Load Oh My Fish configuration.
 source $OMF_PATH/init.fish
 
-witch (uname)
-    case Linux
-         source ./os/linux.fish
-    case Darwin
-              source ./os/darwin.fish
-    case '*'
-            echo Hi, stranger!
-end
-
 set -g default_user "Justin 🐯"
 
+
+
+export WINDOWS_HOST=(grep -oP "(?<=nameserver ).+" /etc/resolv.conf)
+export DISPLAY=$WINDOWS_HOST":0"
+export AWS_REGION="ca-central-1"
+# export RUSTC="/Users/Justin/.cargo/bin/rustc"
+export EDITOR="/usr/local/bin/emacs"
 source ~/.config/fish/secrets/work.fish
 # source ~/.config/fish/secrets/personal.fish
 

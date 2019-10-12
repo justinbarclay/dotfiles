bb3d510ad1b972e76536d0dd2260e32449d96fe6
Justin Barclay <justincbarclay@gmail.com>
2019-10-12 16:24:02 -0600

    Simplify config and consolidate lots of OS specific setup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,16 +1,8 @@
-# Path to Oh My Fish install.
-set -q XDG_DATA_HOME
-  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
-  or set -gx OMF_PATH "$HOME/.local/share/omf"
-
-# Load Oh My Fish configuration.
-source $OMF_PATH/init.fish
-
 switch (uname)
     case Linux
-         source ./os/linux.fish
+         source /home/justin/.config/fish/os/linux.fish
     case Darwin
-              source ./os/darwin.fish
+         source /home/justin/.config/fish/os/darwin.fish
     case '*'
             echo Hi, stranger!
 end
@@ -20,8 +12,4 @@ set -g default_user "Justin 🐯"
 source ~/.config/fish/secrets/work.fish
 # source ~/.config/fish/secrets/personal.fish
 
-
-# Agnoster Customizations specific for Dracula
-set -g theme_display_user yes
-set -g theme_hide_hostname yes
 eval (starship init fish)
--- .config/fish/os/linux.fish
+++ .config/fish/os/linux.fish
@@ -6,20 +6,12 @@ set -q XDG_DATA_HOME
 # Load Oh My Fish configuration.
 source $OMF_PATH/init.fish
 
-set -g default_user "Justin 🐯"
-
-
-
-export WINDOWS_HOST=(grep -oP "(?<=nameserver ).+" /etc/resolv.conf)
+export WINDOWS_HOST=(grep -oP "(?<=nameserver ).+" /etc/resolv.conf | awk '{printf $1; exit}' )
 export DISPLAY=$WINDOWS_HOST":0"
+
 export AWS_REGION="ca-central-1"
-# export RUSTC="/Users/Justin/.cargo/bin/rustc"
-export EDITOR="/usr/local/bin/emacs"
-source ~/.config/fish/secrets/work.fish
-# source ~/.config/fish/secrets/personal.fish
+
+export EDITOR="emacs"
 
 
-# Agnoster Customizations specific for Dracula
-set -g theme_display_user yes
-set -g theme_hide_hostname yes
-eval (starship init fish)
+export locale=en_US.UTF-8

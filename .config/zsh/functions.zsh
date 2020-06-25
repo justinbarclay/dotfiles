be9e68ef0423dd844d89e410ae026c05d5814280
Justin Barclay <justincbarclay@gmail.com>
2020-06-25 08:50:21 -0600

    Add function to fix corrupt zsh history file

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -18,6 +18,17 @@ delete-merged () {
     | xargs git branch -d
 }
 
+# George Ornbo (shapeshed) http://shapeshed.com
+# License - http://unlicense.org
+#
+# Fixes a corrupt .zsh_history file
+
+clean_history() {
+  mv ~/.zsh_history ~/.zsh_history_bad
+  strings ~/.zsh_history_bad > ~/.zsh_history
+  fc -R ~/.zsh_history
+  rm ~/.zsh_history_bad
+}
 #########################
 # Work functions
 #########################

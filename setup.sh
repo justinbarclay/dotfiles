3a54015c3c8666ee7acdf916eb18246210cff23b
Justin Barclay <justincbarclay@gmail.com>
2021-03-03 14:41:35 -0700

    Add xmodmap

--- setup.sh
+++ setup.sh
@@ -3,12 +3,14 @@ script_dir=$(dirname $0 | xargs realpath)
 fish () {
   mkdir -p ~/.config
   ln -s $script_dir/.config/fish ~/.config
+  ln -s $script_dir/.Xmodmap ~/.Xmodmap
 }
 
 zsh () {
   mkdir -p ~/.config
   ln -s $script_dir/.zshrc ~/.zshrc
   ln -s $script_dir/.config/zsh ~/.config
+  ln -s $script_dir/.Xmodmap ~/.Xmodmap
 }
 
 if [ "$1" == "zsh" ]; then

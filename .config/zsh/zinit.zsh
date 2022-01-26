d3fb99dc41813836c6382941b37a6d4c8e2ce3e6
Justin Barclay <justincbarclay@gmail.com>
2022-01-25 20:10:50 -0800

    Update zinit process to reference zdharma-continuum

--- .config/zsh/zinit.zsh
+++ .config/zsh/zinit.zsh
@@ -2,7 +2,7 @@
 if [[ ! -f $HOME/.zinit/bin/zinit.zsh ]]; then
     print -P "%F{33}▓▒░ %F{220}Installing DHARMA Initiative Plugin Manager (zdharma/zinit)…%f"
     command mkdir -p "$HOME/.zinit" && command chmod g-rwX "$HOME/.zinit"
-    command git clone https://github.com/zdharma/zinit "$HOME/.zinit/bin" && \
+    command git clone git@github.com:zdharma-continuum/zinit.git "$HOME/.zinit/bin" && \
         print -P "%F{33}▓▒░ %F{34}Installation successful.%f" || \
         print -P "%F{160}▓▒░ The clone has failed.%f"
 fi
@@ -21,10 +21,10 @@ zplugin ice wait"0" atload"_zsh_autosuggest_start" lucid
 zplugin light zsh-users/zsh-autosuggestions
 
 zplugin ice wait"0" atinit"zpcompinit" lucid
-zplugin light zdharma/fast-syntax-highlighting
+zplugin light zdharma-continuum/fast-syntax-highlighting
 
 zplugin ice wait"1" lucid
-zplugin light zdharma/history-search-multi-word
+zplugin light zdharma-continuum/history-search-multi-word
 
 zplugin ice wait"1" lucid
 zplugin light zsh-users/zsh-history-substring-search

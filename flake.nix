5ac87f97b7ef5e8309848b76016513b8edc7429b
Justin Barclay <git@justinbarclay.ca>
2026-06-09 21:34:17 -0700

    feat: add root flake with graphviz devshell and direnv support

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Root flake for dotfiles";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+    in
+    {
+      devShells = forEachSystem (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              graphviz
+            ];
+          };
+        });
+    };
+}

984bd6405e4ddb341e375ba4d89a312fea7750a5
Justin Barclay <git@justinbarclay.ca>
2026-06-24 22:34:45 -0700

    chore: add tahoe patch from emacs-plus repo

--- home-manager/emacs.nix
+++ home-manager/emacs.nix
@@ -16,11 +16,20 @@ with lib; {
     {
       programs.emacs = {
         enable = true;
-        package = (if pkgs.stdenv.isDarwin then pkgs.emacs-git else pkgs.emacs-igc).override {
-          withSQLite3 = true;
-          withGTK3 = config.modules.emacs.with-gtk;
-          withNativeCompilation = true;
-        };
+        package =
+          let
+            base = (if pkgs.stdenv.isDarwin then pkgs.emacs-git else pkgs.emacs-igc).override {
+              withSQLite3 = true;
+              withGTK3 = config.modules.emacs.with-gtk;
+              withNativeCompilation = true;
+            };
+          in
+          # On macOS 26 (Tahoe) nixpkgs builds against an older SDK, so the
+          # upstream Tahoe scroll-lag fix in nsterm.m is compiled out. Re-enable
+          # it with a runtime version check. See ./patches/tahoe-scroll.patch.
+          base.overrideAttrs (old: {
+            patches = (old.patches or [ ]) ++ lib.optional pkgs.stdenv.isDarwin ./patches/tahoe-scroll.patch;
+          });
         extraPackages =
           (epkgs: [
             (epkgs.treesit-grammars.with-grammars (ps: lib.filter (p: !p.meta.broken) (lib.attrValues ps)))
